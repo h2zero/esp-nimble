67cdf2341b204db1f15fa6da948473bcc34e912d
h2zero <ryan@nable-embedded.io>
2022-07-09 20:03:19 -0600

    Fix clock drivers for nRF devices in Arduino.

--- nimble/drivers/nrf5x/src/ble_phy.c
+++ nimble/drivers/nrf5x/src/ble_phy.c
@@ -2293,7 +2293,7 @@ void ble_phy_disable_dtm(void)
 void
 ble_phy_rfclk_enable(void)
 {
-#if MYNEWT || defined(RIOT_VERSION)
+#if MYNEWT || defined(RIOT_VERSION) || defined(ARDUINO)
 #ifdef NRF52_SERIES
     nrf52_clock_hfxo_request();
 #endif
@@ -2308,7 +2308,7 @@ ble_phy_rfclk_enable(void)
 void
 ble_phy_rfclk_disable(void)
 {
-#if MYNEWT || defined(RIOT_VERSION)
+#if MYNEWT || defined(RIOT_VERSION) || defined(ARDUINO)
 #ifdef NRF52_SERIES
     nrf52_clock_hfxo_release();
 #endif
